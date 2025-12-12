0d26828df70d8e3d66695dd2e47d687eba5006cb
nougzarm <nougzar.ouzoev@hotmail.fr>
2025-12-12 19:11:45 +0100

    feat: implement fmt::Display for the library Error type
    Implement the `core::fmt::Display` trait for the crate's `Error` struct.

--- src/errors.rs
+++ src/errors.rs
@@ -1,5 +1,18 @@
+use core::fmt;
+
 #[derive(Debug, PartialEq)]
 pub enum Error {
     InvalidInputLength,
     InvalidEta,
 }
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::InvalidInputLength => write!(f, "Input length is invalid"),
+            Error::InvalidEta => write!(f, "Invalid value for Eta"),
+        }
+    }
+}
+
+impl core::error::Error for Error {}
